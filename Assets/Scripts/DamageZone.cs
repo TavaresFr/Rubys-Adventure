4c3a35c1aa42578bddb98986cd8143b31b506049
TavaresFr <152510118+TavaresFr@users.noreply.github.com>
2024-04-04 15:53:44 +0100

    Add a Damage Zone and Enemies

--- Assets/Scripts/DamageZone.cs
+++ Assets/Scripts/DamageZone.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DamageZone : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        void OnTriggerStay2D(Collider2D other)
+        void OnTriggerEnter2D(Collider2D other)
         {
             RubyControl controller = other.GetComponent<RubyControl>();
 
